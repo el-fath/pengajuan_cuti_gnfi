5e487acedfa26ad8d35837cae8481ea2d984ff40
yuaneko95 <yuaneko95@gmail.com>
2017-02-20 11:19:34 +0700

    fix error js

--- index.php
+++ index.php
@@ -564,7 +564,7 @@
     <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
     <!-- CORE JQUERY  -->
 <!--     <script src="jquery-2.2.2.js"></script> -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <!-- BOOTSTRAP CORE SCRIPT   -->
     <script src="assets/plugins/bootstrap.min.js"></script>  
      <!-- ISOTOPE SCRIPT   -->
