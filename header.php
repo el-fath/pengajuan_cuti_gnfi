c7781930898877a499af290a1385298e52875c5a
yuaneko95 <yuaneko95@gmail.com>
2017-02-23 10:31:02 +0700

    fiz navbar

--- header.php
+++ header.php
@@ -9,7 +9,9 @@
 <div class="container">
     <div class="navbar navbar-inverse navbar-fixed-top">        
             <div class="navbar-header" >
-            <a class="navbar-brand" href="#" style="padding-top: 0px;"><img src="https://www.goodnewsfromindonesia.id/assets/front/main/logo-gnfi.png" alt="Good News from Indonesia"></a>
+            <a class="navbar-brand" href="#" style="padding-top: 0px;">
+                <img src="https://www.goodnewsfromindonesia.id/assets/front/main/logo-gnfi.png" alt="Good News from Indonesia" style="height: 55px;padding-left: 10%;">
+            </a>
                
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="icon-bar"></span>
@@ -18,7 +20,7 @@
                 </button>
             </div>
             <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
+                <ul class="nav navbar-nav navbar-right" style="padding-right: 130px">
                     <li><a href="#port-sec">Data Master</a></li>
                     <li><a href="ubah_password.php?&id_pegawai=<?php echo $_SESSION['id_pegawai']; ?>">Ganti Password</a></li>
                     <li><a href="proses/logout.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
