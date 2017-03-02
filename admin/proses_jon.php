7ddf8cf7515b8b4427e15231db2194ca60f852cc
el-fath <rochman003@gmail.com>
2017-03-02 17:38:46 +0700

    cuti

--- admin/proses_jon.php
+++ admin/proses_jon.php
@@ -2,12 +2,16 @@
 session_start();
 include 'koneksi.php';
 $username = $_SESSION['username'];
+$id_pegawai = $_SESSION['id_pegawai'];
 $id_pcuti = $_POST['id_pcuti'];
 $tgl_sah = date('Y/m/d');
 $id_jcuti = $_POST['id_jcuti'];
 	
 if ($_POST['id_jcuti'] != '2' ) {
-	$sql = "UPDATE permohonan_cuti SET status = 'disetujui', tgl_sah = '$tgl_sah', disahkan = '$username' WHERE id_pcuti = '$id_pcuti'";
+	if ($_SESSION['is_coordinator'] == '1'){
+		$sql = "UPDATE pegawai_approval_list SET is_approval = '1' WHERE approval_id = '$id_pegawai' AND object_id ='$id_pcuti'"; 
+	}else{echo "<script>alert('salah jon')</script>";}
+	// $sql = "UPDATE permohonan_cuti SET status = 'disetujui', tgl_sah = '$tgl_sah', disahkan = '$username' WHERE id_pcuti = '$id_pcuti'";
 	// $b = mysqli_query($conn, "INSERT into pegawai_approval VALUES ('','$id_pegawai','$tgl_sah')") or die(mysqli_error($conn));
 }else{
 	$sql = "UPDATE pegawai
