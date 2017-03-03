aefcdcfe8d27231bedea713b8d088b8f693e20c6
yuaneko95 <yuaneko95@gmail.com>
2017-03-03 14:48:13 +0700

    error approve barang1

--- approvel.php
+++ approvel.php
@@ -256,6 +256,7 @@
                 $num_rows = mysqli_num_rows($s);
                 if (!empty($num_rows)) {
                 while ($tmp = mysqli_fetch_assoc($s)) {  
+                  $l = mysqli_query($conn,"SELECT * FROM pegawai_approval_list WHERE object_id = '".$tmp['id_pcuti']."'") or die(mysqli_error($conn));
                 $no++
             ?>
             <tr>
@@ -276,7 +277,7 @@
                     <?php } ?>
                 </td>
             <td align="center">
-                    <a href="#" class="btn btn-xs btn-success open_modalbrg <?=$tmp['status'] != 'disetujui' && $tmp['status'] != 'ditolak' && $tmp['grup'] == $_SESSION['grup'] ? '' : 'disabled'?>" id="<?php echo $tmp['id_pbarang'];?>" ><i class="glyphicon glyphicon-check"></i> setujui</a>
+                    <a href="#" class="btn btn-xs btn-success open_modalbrg <?=$tmp['status'] != 'disetujui' && $tmp['status'] != 'ditolak' && $tmp['grup'] == $_SESSION['grup'] && $data['is_approval'] != 1 ? '' : 'disabled'?>" id="<?php echo $tmp['id_pbarang'];?>" ><i class="glyphicon glyphicon-check"></i> setujui</a>
                 </td>
                 <td align="center">
                     <a href="#" class="btn btn-xs btn-danger open_jonbrg <?=$tmp['status'] != 'disetujui' && $tmp['status'] != 'ditolak' && $tmp['grup'] == $_SESSION['grup']? '' : 'disabled'?>" id="<?php echo $tmp['id_pbarang'];?>"><i class="glyphicon glyphicon-remove"></i> Tolak</a>
