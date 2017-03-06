90164aa085a0276b85151ae6ebed0a4d63e6a6b8
yuaneko95 <yuaneko95@gmail.com>
2017-03-06 16:28:27 +0700

    fix denied brg

--- admin/proses_setuju_brg.php
+++ admin/proses_setuju_brg.php
@@ -21,7 +21,7 @@ $a = mysqli_query($conn,"SELECT COUNT(*) AS sisa_approval FROM pegawai_approval_
 $b = mysqli_fetch_assoc($a);
 
 if ($b['sisa_approval'] != 0 ) {
-	$l = mysqli_query($conn,"UPDATE pengadaan_barang SET status = 'disetujui', tgl_sah = '$tgl_sah', disahkan = '$username' WHERE id_pbarang = '$id_pbarang'") or die(mysqli_error($conn));
+	$l = mysqli_query($conn,"UPDATE pengadaan_barang SET status = 'disetujui', tgl_sah = '$tgl_sah' WHERE id_pbarang = '$id_pbarang'") or die(mysqli_error($conn));
 	
 	echo "<script>alert('Penyetujuan Berhasil disetujui')</script>";
 	
