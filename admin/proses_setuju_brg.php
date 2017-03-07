616819a4e6accf39f6869fdce6b27493c0270a1b
yuaneko95 <yuaneko95@gmail.com>
2017-03-07 10:16:00 +0700

    fix approve barang

--- admin/proses_setuju_brg.php
+++ admin/proses_setuju_brg.php
@@ -17,10 +17,10 @@ if ($_SESSION['is_coordinator'] == 1 ) {
 	$s = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 }
 
-$a = mysqli_query($conn,"SELECT COUNT(*) AS sisa_approval FROM pegawai_approval_list WHERE object_id = '$id_pbarang' AND type = 'barang' AND is_approval = '1' ") or die (mysqli_error($conn));
+$a = mysqli_query($conn,"SELECT COUNT(*) AS sisa_approval FROM pegawai_approval_list WHERE object_id = '$id_pbarang' AND type = 'barang' AND is_approval = '0' ") or die (mysqli_error($conn));
 $b = mysqli_fetch_assoc($a);
 
-if ($b['sisa_approval'] != 0 ) {
+if ($b['sisa_approval'] == 0 ) {
 	$l = mysqli_query($conn,"UPDATE pengadaan_barang SET status = 'disetujui', tgl_sah = '$tgl_sah' WHERE id_pbarang = '$id_pbarang'") or die(mysqli_error($conn));
 	
 	echo "<script>alert('Penyetujuan Berhasil disetujui')</script>";
