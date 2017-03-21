8462b86d2913cab668e58980cf0e146f651c1f45
yuaneko95 <yuaneko95@gmail.com>
2017-03-21 17:49:05 +0700

    uji coba edit peg

--- admin/edit_pegawai.php
+++ admin/edit_pegawai.php
@@ -54,7 +54,7 @@
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">ID PEGAWAI <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input type="text" name="id_pegawai" required="required" class="form-control col-md-7 col-xs-12" value="<?php echo $temp['id_pegawai']; ?>">
+                          <input type="text" name="id_pegawai" required="required" class="form-control col-md-7 col-xs-12" value="<?php echo $temp['id_pegawai']; ?>"><?php echo ['id_pegawai']; ?>
                         </div>
                       </div>
                       <div class="form-group">
